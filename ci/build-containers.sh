9fcc37795bb50feb945ac3d1d3bcf720280cb002
Johan Van de Wauw <johan@fluves.com>
2025-06-11 15:25:40 +0200

    Adjust image with fakeroot for image building

--- ci/build-containers.sh
+++ ci/build-containers.sh
@@ -1,7 +1,10 @@
-for i in "debian:bullseye" "debian:bookworm" "ubuntu:focal" "ubuntu:jammy" "ubuntu:noble";
+# ensure login is stored
+#echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+
+for i in  "debian:bookworm" "ubuntu:focal" "ubuntu:jammy" "ubuntu:noble";
 do 
 	podman build --build-arg base_container=$i -t ghcr.io/johanvdw/saga-watem-build-$i .
-	#podman push ghcr.io/johanvdw/saga-watem-build-$i
+	podman push ghcr.io/johanvdw/saga-watem-build-$i
 done;
 
 podman build -f Dockerfile-docs -t ghcr.io/johanvdw/saga-watem-build-docs
